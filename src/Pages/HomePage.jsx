ca6c0a3df20a118f854d44c8cf290bb68189bca6
LucaCiani <lucaciani88@gmail.com>
2025-08-02 13:22:21 +0200

    add backend fetch

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,5 +1,7 @@
 export default function HomePage() {
-    return(
-        <h1>Hello</h1>
-    )
+    const moviesApiUrl = import.meta.env.VITE_BACKEND_URL;
+    fetch(moviesApiUrl)
+        .then((res) => res.json())
+        .then((data) => console.log(data));
+    return <h1>Hello</h1>;
 }
