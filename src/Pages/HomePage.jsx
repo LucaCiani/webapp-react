f27425e75ad2ba2d934d8fa0c035245827b86bc0
LucaCiani <lucaciani88@gmail.com>
2025-08-02 13:30:11 +0200

    fix fetch

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,7 +1,16 @@
+import { useEffect, useState } from "react";
+
 export default function HomePage() {
+    const [movies, setMovies] = useState([]);
     const moviesApiUrl = import.meta.env.VITE_BACKEND_URL;
-    fetch(moviesApiUrl)
-        .then((res) => res.json())
-        .then((data) => console.log(data));
+
+    useEffect(() => {
+        fetch(moviesApiUrl)
+            .then((res) => res.json())
+            .then((data) => setMovies(data));
+    }, []);
+
+    console.log(movies);
+
     return <h1>Hello</h1>;
 }
