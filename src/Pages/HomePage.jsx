c21316ea5cacfdea328856c9b0984417d1759fbe
LucaCiani <lucaciani88@gmail.com>
2025-08-04 16:04:26 +0200

    complete

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -3,9 +3,9 @@ import MoviesList from "../components/MoviesList";
 export default function HomePage() {
     return (
         <>
-            <h1>WELCOME TO MY CINEMA</h1>
+            <h1>🍿WELCOME TO MY CINEMA📽️</h1>
             <p className="h3 mb-5">
-                Here you will find all the trending movies
+                Here you will find all the trending movies🎟️
             </p>
             <MoviesList />
         </>
